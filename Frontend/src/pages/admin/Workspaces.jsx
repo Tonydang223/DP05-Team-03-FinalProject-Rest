0d4fcf969a1f8b12c7184bf16ad22aff7168f3a2
Tshop <thienhngcd18649@fpt.edu.vn>
2023-04-12 16:48:16 +0700

    fix: remove css and resetup for form

--- Frontend/src/pages/admin/Workspaces.jsx
+++ Frontend/src/pages/admin/Workspaces.jsx
@@ -1,42 +1,25 @@
 import React from 'react';
-import { Table } from 'antd';
-import '../../assets/Workspaces.css';
+import { Table, Row, Col } from 'antd';
 
 const dataSource = [
-  {
-    key: '1',
-    name: 'ST Software',
-    state: 'Active',
-    managers: '',
-  },
-  {
-    key: '2',
-    name: 'Devplus',
-    state: 'Inactive',
-    managers: '',
-  },
+  { key: 1, name: 'ST Software', state: 'Active', managers: '' },
+  { key: 2, name: 'Devplus', state: 'Inactive', managers: '' },
 ];
 
 const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'State',
-    dataIndex: 'state',
-    key: 'state',
-  },
-  {
-    title: 'Managers',
-    dataIndex: 'managers',
-    key: 'managers',
-  },
+  { title: 'Name', dataIndex: 'name', key: 'name' },
+  { title: 'State', dataIndex: 'state', key: 'state' },
+  { title: 'Managers', dataIndex: 'managers', key: 'managers' },
 ];
 
 export default function Workspaces() {
   return (
-    <Table dataSource={dataSource} columns={columns} pagination={false} className='custom-table' />
+    <div style={{ marginLeft: '50px' }}>
+      <Row>
+        <Col xs={24} sm={24} md={24} lg={18} xl={18}>
+          <Table dataSource={dataSource} columns={columns} pagination={false} />
+        </Col>
+      </Row>
+    </div>
   );
 }
