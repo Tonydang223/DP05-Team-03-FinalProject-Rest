eb5c6afb5a1e0f807ce46622de14393ca7ba393d
NguyenVanHuy1405 <vanhuy141520@gmail.com>
2023-04-25 08:32:17 +0700

    T09/ManagerMember

--- Frontend/src/pages/auth/Profile/Profile.jsx
+++ Frontend/src/pages/auth/Profile/Profile.jsx
@@ -20,59 +20,43 @@ const Profile = () => {
           margin: '16px 0',
         }}
       >
-        <Breadcrumb.Item>Manager</Breadcrumb.Item>
-        <Breadcrumb.Item>Groups</Breadcrumb.Item>
+        <Breadcrumb.Item>Auth</Breadcrumb.Item>
+        <Breadcrumb.Item>Profile</Breadcrumb.Item>
       </Breadcrumb>
       <div
         style={{
           padding: 24,
           minHeight: 530,
           background: colorBgContainer,
-          display: 'flex',
           justifyContent: 'center',
         }}
       >
-        <Row className="profile-container">
-            <Col xl={12} lg={12} md={12} sm={12} xs={12} className="image-profile">
-                <div className='travelers container section'>
-                  <div className='sectionContainer'>
-                    <div className='travelersContainer grid'>
-                      <div className='singleTraveler'>
-                        <img src={image} className='destinationImage' />
+        <Space direction="vertical">
+          <div className='travelers container section'>
+            <div className='sectionContainer'>
+              <div className='travelersContainer grid'>
+                <div className='singleTraveler'>
+                  <img src={image} className='destinationImage' />
 
-                        <div className='travelerDetails'>
-                          <div className='travelerPicture'>
-                            <img src={Avatar} className='travelerImage' />
-                          </div>
-                          <div className='travelerName'>
-                            <span>VanHuy</span>
-                            <p>vanhuy141520@gmail.com</p>
-                          </div>
-                        </div>
-                      </div>
+                  <div className='travelerDetails'>
+                    <div className='travelerPicture'>
+                      <img src={Avatar} className='travelerImage' />
+                    </div>
+                    <div className='travelerName'>
+                      <span>VanHuy</span>
+                      <p>vanhuy141520@gmail.com</p>
+                      <p className='role'>Staff</p>
                     </div>
                   </div>
                 </div>
-            </Col>
-            <Col xl={12} lg={12} md={12} sm={12} xs={12} className="info-profile">
-              <Title strong className="information">Information</Title>
-            <Space direction="vertical" className="content-info">
-            <Space wrap className="name-profile">
-              <Text><b className='title-name'>Name:</b> User name</Text>
-            </Space>
-            <Space wrap className="email-profile">
-              <Text><b className="title-email">Email:</b> User@gmail.com</Text>
-            </Space>
-            <Space wrap className="role">
-              <Text><b className='title-role'>Role:</b> Staff</Text>
-            </Space>
-            <Space wrap>
-              <Button type="primary" htmlType='submit' className="button-edit">Edit Profile</Button>
-              <Button type="primary" htmlType='submit' className="button-changePassword">Change password</Button>
-            </Space>
-            </Space>  
-            </Col>
-        </Row>
+              </div>
+            </div>
+          </div>
+          <Space wrap>
+            <Button>Edit Profile</Button>
+            <Button>Change password</Button>
+          </Space>
+        </Space>
       </div>
     </Content>
   );
