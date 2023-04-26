86709efdda0d469879534389c82d167bb836424f
Tonydang223 <danghnh@dipro.vn>
2023-04-26 22:36:37 +0700

    fix: add members to group

--- Backend/src/controller/GroupController.js
+++ Backend/src/controller/GroupController.js
@@ -1,23 +1,38 @@
-const { ROLES } = require('../config/constants');
 const GroupModel = require('../model/group.model');
 
 class GroupController {
   async create(req, res) {
-    const { name, id_workspace } = req.body;
+    const { name, id_workspace, masters, members } = req.body;
     try {
       const existedGroup = await GroupModel.findOne({ name });
       if (existedGroup) return res.status(400).json({ message: 'Group already exists!' });
+
+      const checkMembers = (mem) => {
+        if (!mem || !Array.isArray(mem) || mem.length < 1)
+          return res.status(400).json({ message: 'Group must have at least one member!' });
+      };
+
+      checkMembers(members);
+      checkMembers(masters);
       const group = new GroupModel({
         name,
         workspace: id_workspace,
       });
-      const newGroup = await group.save();
-      res.status(200).json({ message: 'Group created successfully', data: { ...newGroup._doc } });
+
+      await group.save();
+
+      const groupAdded = await GroupModel.findByIdAndUpdate(
+        { _id: group._id },
+        { $push: { masters: { $each: masters }, members: { $each: members } } },
+        { new: true },
+      ).populate('members masters', '-password');
+
+      res.status(200).json({ message: 'Group created successfully', data: { ...groupAdded._doc } });
     } catch (error) {
       res.status(500).send({ message: error.message });
     }
   }
-  async addMembers(req, res) {
+  async updateGroup(req, res) {
     try {
       const { masters, members, name } = req.body;
       const existedMembers = await GroupModel.findOne({
@@ -25,14 +40,10 @@ class GroupController {
       });
       const arrayIds = (arr, fieldArr) => {
         if (Array.isArray(arr) && arr.length > 0) {
-          return arr
-            .filter((v) => ![ROLES[0], ROLES[1]].includes(v.role))
-            .map((i) => i._id)
-            .filter((member) =>
-              existedMembers._doc[`${fieldArr}`]?.length === 0
-                ? member
-                : !existedMembers._doc[`${fieldArr}`]?.includes(member),
-            );
+          return arr.filter(
+            (member) =>
+              !existedMembers._doc[`${fieldArr}`]?.map((v) => v.toString())?.includes(member),
+          );
         }
       };
       const masterIds = arrayIds(masters, 'masters');
@@ -40,9 +51,12 @@ class GroupController {
 
       if (!existedMembers) return res.status(400).json({ message: 'Group not found!' });
 
-      const addedUsers = await GroupModel.findOneAndUpdate(
-        { _id: req.params.id },
-        { $push: { masters: { $each: masterIds }, members: { $each: memberIds } }, $set: { name } },
+      const addedUsers = await GroupModel.findByIdAndUpdate(
+        { _id: existedMembers._id },
+        {
+          $push: { masters: { $each: masterIds }, members: { $each: memberIds } },
+          $set: { name },
+        },
         { new: true },
       ).populate('masters members', '-password');
 
--- Backend/src/routes/group.routes.js
+++ Backend/src/routes/group.routes.js
@@ -5,7 +5,7 @@ const GroupController = require('../controller/GroupController');
 const router = express.Router();
 
 router.post('/create', userAuthPermission([ROLES[1]]), GroupController.create);
-router.post('/update/:id', userAuthPermission([ROLES[1]]), GroupController.addMembers);
+router.post('/update/:id', userAuthPermission([ROLES[1]]), GroupController.updateGroup);
 router.get('/getAll', userAuthPermission(), GroupController.getGroups);
 router.get('/getDetail/:id', userAuthPermission(), GroupController.getDetailGroup);
 
